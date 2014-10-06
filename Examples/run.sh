6af224f9370a0d8f9a64a11ecef878cfd54f9678
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-10-06 19:30:55 +0100

    Adds MPI and OpenMP Example

--- Examples/run.sh
+++ Examples/run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+echo '**********'
+echo 'Compiling examples'
+echo '**********'
 make
+echo '**********'
+echo 'Running Single Threaded MPI Hello World'
+echo '**********'
 mpiexec -f ~/LES-WRF-MPI/machinefile -n 8 ~/LES-WRF-MPI/Examples/helloMPI
+echo '**********'
+echo 'Running Single Node OpenMP Hello World'
+echo '**********'
 ~/LES-WRF-MPI/Examples/helloOpenMP
+echo '**********'
+echo 'Running MPI OpenMP Hello World'
+echo '**********'
+mpiexec -f ~/LES-WRF-MPI/machinefilemt -n 4 ~/LES-WRF-MPI/Examples/helloMPIOpenMP
+echo '**********'
 
