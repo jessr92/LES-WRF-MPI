4801a33a7c227d5478f15ef21813df72d3421ab8
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-11-18 17:28:21 +0000

    Disable broken example

--- Examples/helloMPI.f95
+++ Examples/helloMPI.f95
@@ -7,7 +7,7 @@ contains
 
 subroutine main()
     implicit none
-    integer :: rank, size, ierror, test, source, destination, tag, elements
+    integer(4) :: rank, size, ierror, test, recv, source, destination, tag, elements
     call MPI_INIT(ierror)
     call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
     call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
@@ -16,15 +16,21 @@ subroutine main()
     tag = 1234
     test = 0
     elements = 1
+
+    print*, 'rank ', rank, ' size ', size
+
     if (rank .eq. source) then
         test = 1
-        call MPI_SEND(test, elements, MPI_INTEGER, destination, tag, MPI_COMM_WORLD, ierror)
+        !call MPI_SEND(test, elements, MPI_INTEGER, destination, tag, MPI_COMM_WORLD, ierror)
     end if
     if (rank .eq. destination) then
-        call MPI_RECV(test, elements, MPI_INTEGER, source, tag, MPI_COMM_WORLD, ierror)
+        !call MPI_RECV(recv, elements, MPI_INTEGER, source, tag, MPI_COMM_WORLD, ierror)
+        print*, 'node', rank, ': received ', recv
     end if
     print*, 'node', rank, ': Hello world'
-    call MPI_FINALIZE(ierror)
+    if (rank .eq. 1) then
+        call MPI_FINALIZE(ierror)
+    end if
 end subroutine main
 
 end program helloMPI
