4e0ee81aff287250201f612e602c21ee149c9acd
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-11-21 16:04:51 +0000

    Tidy up params_common_sn to make it easier for me to read

--- LES/F95Sources/params_common_sn.f95
+++ LES/F95Sources/params_common_sn.f95
@@ -1,10 +1,11 @@
 module params_common_sn
-    integer, parameter  :: ipmax = 150;       integer, parameter  :: jpmax = 150
-    !integer, parameter  :: ipmax = 254;       integer, parameter  :: jpmax = 253
+    integer, parameter :: ipmax = 150, jpmax = 150
+    !integer, parameter :: ipmax = 254, jpmax = 253
 #ifndef TEST_SMALL_DOMAIN
-      integer, parameter  :: ip = 150;       integer, parameter  :: jp = 150;       integer, parameter  :: kp = 90
-      !integer, parameter  :: ip = 254;       integer, parameter  :: jp = 253;       integer, parameter  :: kp = 94
-#else      
-      integer, parameter  :: ip = 25;       integer, parameter  :: jp = 25;       integer, parameter  :: kp = 90
-#endif      
+    integer, parameter :: ip = 150, jp = 150, kp = 90
+    !integer, parameter :: ip = 254, jp = 253, kp = 94
+#else
+    integer, parameter :: ip = 25, jp = 25, kp = 90
+#endif
 end module params_common_sn
+
