009876ddda18d2e99308738509e611178781d3a6
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-11-24 22:17:46 +0000

    Simplify real array output code

--- LES/F95Sources/mpi_helper.f95
+++ LES/F95Sources/mpi_helper.f95
@@ -336,11 +336,7 @@ subroutine outputArrayReal(array)
     integer :: col, row
     do row = 1, size(array, 1)
         do col = 1, size(array,2)
-            if (array(row, col) .ne. -1) then
-                write(*,"(F5.2)",advance="no") array(row,col)
-            else
-                write(*,"(A4)",advance="no") '-'
-            end if
+            write(*,"(F5.2)",advance="no") array(row,col)
         end do
         write (*,*)
     end do
