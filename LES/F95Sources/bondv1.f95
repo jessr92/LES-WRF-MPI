32e44a1687ef6a2d584cf26daf80d21f49fa861e
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-11-26 18:33:29 +0000

    Finish bondv1 boundary conditions

--- LES/F95Sources/bondv1.f95
+++ LES/F95Sources/bondv1.f95
@@ -14,7 +14,7 @@ subroutine bondv1(jm,u,z2,dzn,v,w,km,n,im,dt,dxs)
     real(kind=4), dimension(0:ip+1,-1:jp+1,-1:kp+1) , intent(InOut) :: w
     real(kind=4), dimension(kp+2) , intent(In) :: z2
     real(kind=4) :: u_val
-    integer :: i, j, k
+    integer :: i, j, k, startI, endI
     real(kind=4) :: aaa, bbb, uout
 ! 
 ! 
@@ -22,34 +22,55 @@ subroutine bondv1(jm,u,z2,dzn,v,w,km,n,im,dt,dxs)
 ! 
 !      Setup for initial wind profile
 ! 
-    do i = 0,1
-        do k = 1,78 ! kp = 90 so OK
-            do j = 1,jm
-                u_val = 5.*((z2(k)+0.5*dzn(k))/600.)**0.2
-                !print *, u_val
-                u(i,j,k) = u_val
-                v(i,j,k) = 0.0
-                w(i,j,k) = 0.0
+#ifdef MPI
+    if (isTopRow(procPerRow)) then
+#endif
+        do i = 0,1
+            do k = 1,78 ! kp = 90 so OK
+                do j = 1,jm
+                    u_val = 5.*((z2(k)+0.5*dzn(k))/600.)**0.2
+                    !print *, u_val
+                    u(i,j,k) = u_val
+                    v(i,j,k) = 0.0
+                    w(i,j,k) = 0.0
+                end do
             end do
         end do
-    end do
 
-    do i = 0,1
-        do k = 79,km
-            do j = 1,jm
-                u(i,j,k) = u(i,j,77)
-                v(i,j,k) = 0.0
-                w(i,j,k) = 0.0
+        do i = 0,1
+            do k = 79,km
+                do j = 1,jm
+                    u(i,j,k) = u(i,j,77)
+                    v(i,j,k) = 0.0
+                    w(i,j,k) = 0.0
+                end do
             end do
         end do
-    end do
-    
+#ifdef MPI
+    end if
+#endif
+
 #if ICAL == 0
     !if(ical == 0.and.n == 1) then
     if(n == 1) then
         do k = 1,km
             do j = 1,jm
-                do i = 2,im
+#ifdef MPI
+                if (isTopRow(procPerRow)) then
+                    startI = 2
+                else
+                    startI = 0
+                end if
+                if (isBottomRow(procPerRow)) then
+                    endI = ip
+                else
+                    endI = ip + 1
+                end if
+#else
+                startI = 2
+                endI = im
+#endif
+                do i = startI, endI
                     u(i,j,k) = u(1,j,k)
                     v(i,j,k) = v(1,j,k)
                     w(i,j,k) = w(1,j,k)
@@ -66,37 +87,43 @@ subroutine bondv1(jm,u,z2,dzn,v,w,km,n,im,dt,dxs)
 ! ------------- outflow condition ------------
 !      advective condition
 ! 
-    aaa = 0.0
-    bbb = 0.0
-    do k = 1,km
-        do j = 1,jm
-            aaa = amax1(aaa,u(im,j,k))
-            bbb = amin1(bbb,u(im,j,k))
+#ifdef MPI
+    if (isBottomRow(procPerRow)) then
+#endif
+        aaa = 0.0
+        bbb = 0.0
+        do k = 1,km
+            do j = 1,jm
+                aaa = amax1(aaa,u(im,j,k))
+                bbb = amin1(bbb,u(im,j,k))
+            end do
         end do
-    end do
-    uout = (aaa+bbb)/2.
+        uout = (aaa+bbb)/2.
 #ifdef WV_DEBUG
-    print *, 'F95: UOUT: ',uout
+        print *, 'F95: UOUT: ',uout
 #endif
 
-    do k = 1,km
-        do j = 1,jm
-            u(im,j,k) = u(im,j,k)-dt*uout *(u(im,j,k)-u(im-1,j,k))/dxs(im)
+        do k = 1,km
+            do j = 1,jm
+                u(im,j,k) = u(im,j,k)-dt*uout *(u(im,j,k)-u(im-1,j,k))/dxs(im)
+            end do
         end do
-    end do
 
-    do k = 1,km
-        do j = 1,jm
-            v(im+1,j,k) = v(im+1,j,k)-dt*uout *(v(im+1,j,k)-v(im,j,k))/dxs(im)
+        do k = 1,km
+            do j = 1,jm
+                v(im+1,j,k) = v(im+1,j,k)-dt*uout *(v(im+1,j,k)-v(im,j,k))/dxs(im)
+            end do
         end do
-    end do
 
-    do k = 1,km
-        do j = 1,jm
-            w(im+1,j,k) = w(im+1,j,k)-dt*uout *(w(im+1,j,k)-w(im,j,k))/dxs(im)
+        do k = 1,km
+            do j = 1,jm
+                w(im+1,j,k) = w(im+1,j,k)-dt*uout *(w(im+1,j,k)-w(im,j,k))/dxs(im)
+            end do
         end do
-    end do
-      
+#ifdef MPI
+    end if
+#endif
+#if !defined(MPI) || (PROC_PER_ROW==1)
 ! --side flow condition; periodic
     do k = 0,km+1
         do i = 0,im+1
@@ -118,7 +145,29 @@ subroutine bondv1(jm,u,z2,dzn,v,w,km,n,im,dt,dxs)
             w(i,jm+1,k) = w(i,   1,k)
         end do
     end do 
-
+#else
+    if (isLeftMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(u, size(u, 1) - 2, size(u, 2) - 2, size(u, 3), procPerRow, 1)
+        call sideLeftToRightMPIAllExchange(u, size(u, 1) - 2, size(u, 2) - 2, size(u, 3), procPerRow, 2)
+    else if (isRightMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(u, size(u, 1) - 2, size(u, 2) - 2, size(u, 3), procPerRow, jp+1)
+        call sideLeftToRightMPIAllExchange(u, size(u, 1) - 2, size(u, 2) - 2, size(u, 3), procPerRow, jp+2)
+    end if
+    if (isLeftMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(v, size(v, 1) - 2, size(v, 2) - 2, size(v, 3), procPerRow, 1)
+        call sideLeftToRightMPIAllExchange(v, size(v, 1) - 2, size(v, 2) - 2, size(v, 3), procPerRow, 2)
+    else if (isRightMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(v, size(v, 1) - 2, size(v, 2) - 2, size(v, 3), procPerRow, jp+1)
+        call sideLeftToRightMPIAllExchange(v, size(v, 1) - 2, size(v, 2) - 2, size(v, 3), procPerRow, jp+2)
+    end if
+    if (isLeftMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(w, size(w, 1) - 2, size(w, 2) - 2, size(w, 3), procPerRow, 1)
+        call sideLeftToRightMPIAllExchange(w, size(w, 1) - 2, size(w, 2) - 2, size(w, 3), procPerRow, 2)
+    else if (isRightMostColumn(procPerRow)) then
+        call sideRightToLeftMPIAllExchange(w, size(w, 1) - 2, size(w, 2) - 2, size(w, 3), procPerRow, jp+1)
+        call sideLeftToRightMPIAllExchange(w, size(w, 1) - 2, size(w, 2) - 2, size(w, 3), procPerRow, jp+2)
+    end if
+#endif
 ! -------top and underground condition
     do j = 0,jm+1
         do i = 0,im+1
