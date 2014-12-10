145d2c2928157d6004f1b28e5ac3998d28ce2608
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-12-10 13:04:44 +0000

    Fix vw_debug instead of wv_debug mistake

--- LES/F95Sources/compile.sh
+++ LES/F95Sources/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-scons ocl=0 mpi=0 verbose=1 gr_debug=1 vw_debug=1
+scons ocl=0 mpi=0 verbose=1 gr_debug=1 wv_debug=1
 mv les_main les_main_debug
 
-scons ocl=0 mpi=1 verbose=1 gr_debug=1 vw_debug=1
+scons ocl=0 mpi=1 verbose=1 gr_debug=1 wv_debug=1
 mv les_main_mpi les_main_mpi_debug
 
 scons ocl=0 mpi=0 verbose=1
