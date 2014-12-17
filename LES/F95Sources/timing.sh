c8870ef93b121e26f2caa945dbb8fd7707a3c56c
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2014-12-17 17:20:14 +0000

    Fix missing / in mpi shared memory output directory

--- LES/F95Sources/timing.sh
+++ LES/F95Sources/timing.sh
@@ -26,7 +26,7 @@ do
     do
         PROCESSES=`expr $procPerCol \\* $procPerRow`
         if [ $PROCESSES -le $HARDWARE_THREAD_COUNT ]; then
-            OUTPUT_FILE=$TIMING_DIRECTORY"MPI_SharedMemory/les_main_mpi_row"$procPerRow"_col"$procPerCol".txt"
+            OUTPUT_FILE=$TIMING_DIRECTORY"/MPI_SharedMemory/les_main_mpi_row"$procPerRow"_col"$procPerCol".txt"
             scons ocl=0 mpi=1 D=TIMINGS procPerRow=$procPerRow procPerCol=$procPerCol
             mpiexec -np $PROCESSES ./les_main_mpi > $OUTPUT_FILE
         fi
