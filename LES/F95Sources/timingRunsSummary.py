3f6f3288da7816bbde1164d54712bf96f07e3d85
Gordon Reid <1002536r@student.gla.ac.uk>
2015-01-07 12:47:47 +0000

    Timing summary now supports expanding area logs

--- LES/F95Sources/timingRunsSummary.py
+++ LES/F95Sources/timingRunsSummary.py
@@ -2,10 +2,17 @@ import os
 import re
 import sys
 
+# Known Limitations:
+# 1. Doesn't deal with multiple hosts
+# 2. Doesn't deal with multiple MPI Versions on a single host
+# 3. Only supports les_main.txt, les_main_ifbf=0.txt, MPI_SharedMemory/*.txt, and MPI_SharedMemoryExpandingArea/*.txt
+
 fileList = []
-rootDir = "timingRuns"
+rootDir = sys.argv[1] # For example, timingRuns/<hostname>
 originalRuntime = 0.0
+originalIFBF0Runtime = 0.0
 mpiRuns = {}
+mpiExpandingAreaRuns = {}
 
 for root, subFolders, files in os.walk(rootDir):
     for filename in files:
@@ -24,18 +31,41 @@ for path in fileList:
         if re.match(" Total time:", line):
             floats = re.search("\d+.\d+", line)
             runtime = max(runtime, float(floats.group()))
-    if len(path.split("/")) == 4:
+    # Found the runtime for the original code
+    if path.endswith("les_main.txt"):
         if originalRuntime != 0.0:
             print("Error, two original runs found")
             sys.exit(-1)
         originalRuntime = runtime
-    else:
+    # Found the runtime for the original code but with IFBF=0
+    if path.endswith("les_main_ifbf0.txt"):
+        if originalIFBF0Runtime != 0.0:
+            print("Error, two ifbf=0 runs found")
+            sys.exit(-1)
+        originalIFBF0Runtime = 0.0
+    # Found a runtime for the MPI code where total area is 150x150x90
+    if "/MPI_SharedMemory/" in path:
         try:
             mpiRuns[processCount] = min(mpiRuns[processCount], runtime)
         except KeyError:
             mpiRuns[processCount] = runtime
+    # Found a runtime for the MPI code where each process has its own 150x150x90 area
+    if "/MPI_SharedMemoryExpandingArea/" in path:
+        try:
+            mpiExpandingAreaRuns[processCount] = min(mpiExpandingAreaRuns[processCount], runtime)
+        except KeyError:
+            mpiExpandingAreaRuns[processCount] = runtime
 
-print("Original single threaded code executed in " + str(originalRuntime) + " seconds")
+if originalRuntime != 0.0:
+    print("Original single threaded code, " + str(originalRuntime))
 
+if originalIFBF0Runtime != 0.0:
+    print("Original single threaded ifbf=0 code, " + str(originalIFBF0Runtime))
+
+print("\nMPI_SharedMemory Runs")
 for key in mpiRuns.keys():
     print(str(key) + ", " + str(mpiRuns[key]))
+
+print("\nMPI_SharedMemoryExpandingArea Runs")
+for key in mpiExpandingAreaRuns.keys():
+    print(str(key) + ", " + str(mpiExpandingAreaRuns[key]))
