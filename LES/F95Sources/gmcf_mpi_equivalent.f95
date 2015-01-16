73146fb9690c21c525af40a0ecc3e17a934e4983
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-01-16 16:21:35 +0000

    Adds skeleton for Send/Recv 3DFloatArray

--- LES/F95Sources/gmcf_mpi_equivalent.f95
+++ LES/F95Sources/gmcf_mpi_equivalent.f95
@@ -123,6 +123,44 @@ subroutine GMCF_MPI_Recv1DFloatArray()
     ! GMCF_MPI_Wait()
 end subroutine GMCF_MPI_Recv1DFloatArray
 
+subroutine GMCF_MPI_ISend3DFloatArray(rank, array, tag, destination, time)
+    implicit none
+    integer, intent(in) :: rank, tag, destination, time
+    real, dimension(:,:,:) :: array
+    integer :: pre_post
+    pre_post = PRE ! Arbitrary
+    call gmcfSend3DFloatArray(rank, array, size(array), tag, destination, pre_post, time)
+end subroutine GMCF_MPI_ISend3DFloatArray
+
+subroutine GMCF_MPI_Send3DFloatArray(rank, array, tag, destination, time)
+    implicit none
+    integer, intent(in) :: rank, tag, destination, time
+    real, dimension(:,:,:) :: array
+    ! Equivalent to
+    ! GMCF_MPI_ISend3DFloatArray
+    ! No wait call required as the GMCF framework makes a copy of array so it
+    ! is safe to reuse array straight away even though there is no blocking
+    ! call.
+    call GMCF_MPI_ISend3DFloatArray(rank, array, tag, destination, time)
+end subroutine GMCF_MPI_Send3DFloatArray
+
+subroutine GMCF_MPI_IRecv3DFloatArray()
+    implicit none
+    ! Equivalent to
+    ! Nothing in this case? Within models doesn't need to request data from
+    ! the sender unlike between models which does. Also MPI doesn't guarantee
+    ! IRecv actually has any valid data until after MPI_Wait() returns. Since
+    ! GMCF doesn't give unique labels to each request then there is nothing to
+    ! do here...
+end subroutine GMCF_MPI_IRecv3DFloatArray
+
+subroutine GMCF_MPI_Recv3DFloatArray()
+    implicit none
+    ! Equivalent to
+    call GMCF_MPI_IRecv3DFloatArray()
+    ! GMCF_MPI_Wait()
+end subroutine GMCF_MPI_Recv3DFloatArray
+
 subroutine GMCF_MPI_Wait()
     implicit none
     ! Equivalent to
