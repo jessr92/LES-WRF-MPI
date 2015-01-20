ac4a10aa4d3a7d7c9f8bebfe8134999e5c60ffea
Gordon Reid <1002536r@student.gla.ac.uk>
2015-01-20 18:49:41 +0000

    Minor formatting fix in td_generator

--- GMCF_Utilities/td_generator.py
+++ GMCF_Utilities/td_generator.py
@@ -3,12 +3,15 @@ import sys
 INDENTATION_STEPPING = 4
 indentationLevel = 0
 
+
 def indented_output(message):
     print((" " * indentationLevel) + message)
+
 
 def indentation_increase():
     global indentationLevel
     indentationLevel += INDENTATION_STEPPING
+
 
 def indentation_decrease():
     global indentationLevel
