7cde7ec06cbaa5ebd8b818fa7619156eecabc720
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-01-23 19:00:33 +0000

    Simplifies halo example to make it easier to debug

--- Examples/GMCF_Halo_Example/src/GMCF/Models/haloExchange3DRealExample.f95
+++ Examples/GMCF_Halo_Example/src/GMCF/Models/haloExchange3DRealExample.f95
@@ -22,46 +22,7 @@ subroutine program_haloExchange3DRealExample(sys, tile, model_id) ! This replace
     allocate(processArray(rowCount + topThickness + bottomThickness, &
                           colCount + leftThickness + rightThickness, &
                           depthSize))
-    call initArray(processArray, leftThickness, rightThickness, topThickness, bottomThickness, rank)
-    call exchangeRealHalos(processArray, procPerRow, leftThickness, rightThickness, topThickness, bottomThickness)
+    print *, 'Hello from ', i
     deallocate(processArray)
 end subroutine program_haloExchange3DRealExample
-
-subroutine initArray(processArray, leftThickness, rightThickness, topThickness, bottomThickness, rank)
-    implicit none
-    real(kind=4), dimension(:,:,:), intent(out) :: processArray
-    integer, intent(in) :: leftThickness, rightThickness, topThickness, bottomThickness, rank
-    integer :: col, row, depth
-    do row = 1, size(processArray, 1)
-        do col = 1, size(processArray, 2)
-            do depth = 1, size(processArray, 3)
-                processArray(row, col, depth) = -1.00
-            end do
-        end do
-    end do
-    do row = topThickness + 1, size(processArray, 1) - bottomThickness
-        do col = leftThickness + 1, size(processArray, 2) - rightThickness
-            do depth = 1, size(processArray, 3)
-                processArray(row, col, depth) = rank
-                if (col < rightThickness + leftThickness + 1 .or. &
-                    col > size(processArray, 2) - rightThickness - leftThickness .or. &
-                    row < topThickness + bottomThickness + 1 .or. &
-                    row > size(processArray, 1) - bottomThickness - topThickness) then
-                    processArray(row, col, depth) = processArray(row, col, depth) + 0.5
-                end if
-            end do
-        end do
-    end do
-end subroutine initArray
-
-subroutine exchangeRealHalos(array, procPerRow, leftThickness, &
-                                rightThickness, topThickness, &
-                                bottomThickness)
-    implicit none
-    real(kind=4), dimension(:,:,:), intent(inout) :: array
-    integer, intent(in) :: procPerRow, leftThickness, rightThickness, topThickness, bottomThickness
-    integer :: i, commWith, r, c, d, rowCount, colCount, depthSize, requests(8)
-    real(kind=4), dimension(:,:,:), allocatable :: leftRecv, leftSend, rightSend, rightRecv
-    real(kind=4), dimension(:,:,:), allocatable :: topRecv, topSend, bottomSend, bottomRecv
-end subroutine exchangeRealHalos
 
