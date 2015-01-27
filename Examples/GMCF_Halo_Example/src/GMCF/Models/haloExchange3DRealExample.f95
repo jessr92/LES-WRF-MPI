eb68e959a804a6ab531c3ec6b9ef2858c681fe26
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-01-27 12:08:47 +0000

    Adds call to get tile id so each thread for one model can be distinguished

--- Examples/GMCF_Halo_Example/src/GMCF/Models/haloExchange3DRealExample.f95
+++ Examples/GMCF_Halo_Example/src/GMCF/Models/haloExchange3DRealExample.f95
@@ -4,6 +4,10 @@ subroutine program_haloexchange3drealexample(sys, tile, model_id) ! This replace
     integer(8) , intent(In) :: sys
     integer(8) , intent(In) :: tile
     integer , intent(In) :: model_id
-    print *, 'Hello from ', model_id
+    integer :: tile_id
+    call gmcfInitCoupler(sys, tile, model_id)
+    call gmcfGetTileId(tile, tile_id)
+    print *, 'Hello from ', model_id, ' and tile_id ', tile_id
+    call gmcfFinished(model_id)
 end subroutine program_haloexchange3drealexample
 
