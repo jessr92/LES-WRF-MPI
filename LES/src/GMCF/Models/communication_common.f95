01e630b7500f481465f2814cb4ec36a8025d5d29
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-02-10 15:10:37 +0000

    Adds globalSumOfGMCF

--- LES/src/GMCF/Models/communication_common.f95
+++ LES/src/GMCF/Models/communication_common.f95
@@ -5,6 +5,7 @@ integer, parameter :: zbmTag = 5
 integer, parameter :: leftSideTag = 6, rightSideTag = 7
 integer, parameter :: dxTag = 8, dyTag = 9, collect3DReal4Tag = 10
 integer, parameter :: topLeftTag = 11, topRightTag = 12, bottomLeftTag = 13, bottomRightTag = 14
+integer, parameter :: globalSumTag = 15, globalMaxTag = 16, globalMinTag = 17
 integer, parameter :: leftNeighbour = 1, rightNeighbour = 2, topNeighbour = 3, bottomNeighbour = 4
 end module
 
