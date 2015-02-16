9d314afe7b240bfaad012a32a8fc025a54a59148
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-02-16 13:31:26 +0000

    Auto generated fix for non-convergence

--- LES/src/GMCF/Models/fortran_helper.f95
+++ LES/src/GMCF/Models/fortran_helper.f95
@@ -27,6 +27,15 @@ subroutine zero2DReal4Array(array)
     end do
 end subroutine zero2DReal4Array
 
+subroutine zero1DReal4Array(array)
+    implicit none
+    real(kind=4), dimension(:), intent(inout) :: array
+    integer :: i
+    do i=1, size(array,1)
+        array(i) = 0.0
+    end do
+end subroutine zero1DReal4Array
+
 subroutine outputArray(array)
     implicit none
     integer, dimension(:,:), intent(in) :: array
