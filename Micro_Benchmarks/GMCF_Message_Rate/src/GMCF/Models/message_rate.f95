9278af858906decd04e5e3dcc55bee09840bbd5e
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-02-18 19:44:37 +0000

    Adds throughput to micro benchmarks

--- Micro_Benchmarks/GMCF_Message_Rate/src/GMCF/Models/message_rate.f95
+++ Micro_Benchmarks/GMCF_Message_Rate/src/GMCF/Models/message_rate.f95
@@ -5,11 +5,12 @@ subroutine program_message_rate(sys, tile, model_id) ! This replaces 'program ma
     integer(8) , intent(In) :: tile
     integer , intent(In) :: model_id
     integer :: iterations, i, has_packets, fifo_empty
-    integer :: clock_start, clock_end, clock_rate
-    real(kind=4) :: total_time, messages_per_second
+    integer :: clock_start, clock_end, clock_rate, elements
+    real(kind=4) :: total_time, messages_per_second, throughput
     type(gmcfPacket) :: packet
     real(kind=4), dimension(1) :: buffer
     iterations = 100000
+    elements = size(buffer)
     call gmcfInitCoupler(sys, tile, model_id)
     call system_clock(clock_start, clock_rate)
     if (mod(model_id, 2) .eq. 0) then
@@ -59,7 +60,9 @@ subroutine program_message_rate(sys, tile, model_id) ! This replaces 'program ma
     if (mod(model_id, 2) .eq. 0) then
         total_time = (clock_end - clock_start)/real(clock_rate)
         messages_per_second = iterations / total_time
+        throughput = (iterations * elements * 4) / total_time
         print*, total_time, "s for ", iterations, " messages, a rate of ", messages_per_second, " messages per second."
+        print*, "Message size is ", elements, " reals so there is a throughput of ", throughput/1024, "KB/s"
     end if
 end subroutine program_message_rate
 
--- Micro_Benchmarks/MPI_Message_Rate/message_rate.f95
+++ Micro_Benchmarks/MPI_Message_Rate/message_rate.f95
@@ -2,11 +2,12 @@ program message_rate
 use mpi
 integer :: rank, ierror, mpi_size, status(MPI_STATUS_SIZE)
 integer :: iterations, i
-integer :: clock_start, clock_end, clock_rate
-real(kind=4) :: total_time, messages_per_second
+integer :: clock_start, clock_end, clock_rate, elements
+real(kind=4) :: total_time, messages_per_second, throughput
 real(kind=4), dimension(1) :: buffer
 iterations = 10000000
 
+elements = size(buffer)
 call MPI_Init(ierror)
 call MPI_Comm_Rank(MPI_COMM_WORLD, rank, ierror)
 call MPI_Comm_Size(MPI_COMM_WORLD, mpi_size, ierror)
@@ -32,7 +33,9 @@ call MPI_Finalize(ierror)
     if (mod(rank, 2) .eq. 0) then
         total_time = (clock_end - clock_start)/real(clock_rate)
         messages_per_second = iterations / total_time
+        throughput = (iterations * elements * 4) / total_time
         print*, total_time, "s for ", iterations, " messages, a rate of ", messages_per_second, " messages per second."
+        print*, "Message size is ", elements, " reals so there is a throughput of ", throughput/1024, "KB/s"
     end if
 end program message_rate
 
