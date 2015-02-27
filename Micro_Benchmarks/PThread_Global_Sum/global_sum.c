ac331e77d9d76c3ce9e1e0ac1974201efee5c4da
Gordon Reid <1002536r@student.gla.ac.uk>
2015-02-27 18:18:33 +0000

    Beginning of pthread global sum microbenchmark

---
+++ Micro_Benchmarks/PThread_Global_Sum/global_sum.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define THREAD_COUNT 4
+#define ITERATIONS 1
+
+int thread_ids[THREAD_COUNT];
+pthread_t threads[THREAD_COUNT];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_MUTEX_INITIALIZER;
+int count = 0;
+int opResult = 0;
+
+int reduce(int value) {
+    pthread_mutex_lock(&mutex);
+    printf("Doing the reduce\n");
+    opResult += value;
+    count--;
+    if (count == 0) {
+        pthread_cond_broadcast(&cond);
+    } else {
+        while (count != 0) {
+            printf("pthread cond wait:%d\n", count);
+            pthread_cond_wait(&cond, &mutex);
+            printf("I'm awake again:%d\n", count);
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+    return opResult;
+}
+
+int opAsMaster(int i) {
+    pthread_mutex_lock(&mutex);
+    count = THREAD_COUNT;
+    opResult = 0;
+    pthread_mutex_unlock(&mutex);
+    pthread_cond_broadcast(&cond);
+    printf("master going to do the reduce\n");
+    return reduce(i);
+}
+
+int opAsNonMaster(int i) {
+    pthread_mutex_lock(&mutex);
+    while (count == 0) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    pthread_mutex_unlock(&mutex);
+    printf("going to do the reduce\n");
+    return reduce(i);
+}
+
+void *globalSum(void* thread_id) {
+    int id = (int)thread_id;
+    int result = 0;
+    for (int i=0; i < ITERATIONS; i++) {
+        result = (id == 0) ? opAsMaster(id) : opAsNonMaster(id);
+    }
+    printf("%d\n", result);
+}
+
+void main() {
+    pthread_t thread;
+    int returnValue;
+    struct timeval start, end;
+    for (int i=1; i < THREAD_COUNT; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, globalSum, (void *) thread_ids[i]);
+    }
+    gettimeofday(&start, 0);
+    globalSum(0);
+    for (int i=1; i < THREAD_COUNT; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    gettimeofday(&end, 0);
+    long long elapsed_time = ((end.tv_sec - start.tv_sec) * 1000000LL) + end.tv_usec - start.tv_usec;
+    double seconds = elapsed_time / 1000000.0;
+    printf("%f\n", seconds);
+}
