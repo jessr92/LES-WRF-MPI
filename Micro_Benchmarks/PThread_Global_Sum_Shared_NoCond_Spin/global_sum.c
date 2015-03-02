1a1cd690d33108029cb9e5331d22931620d2fbe7
Gordon Reid <1002536r@student.gla.ac.uk>
2015-03-02 16:03:11 +0000

    Adds pthread_spin_destroy

--- Micro_Benchmarks/PThread_Global_Sum_Shared_NoCond_Spin/global_sum.c
+++ Micro_Benchmarks/PThread_Global_Sum_Shared_NoCond_Spin/global_sum.c
@@ -26,6 +26,10 @@ int pthread_spin_init(pthread_spinlock_t *lock, int pshared) {
     return 0;
 }
 
+int pthread_spin_destroy(pthread_spinlock_t *lock) {
+    return 0;
+}
+
 int pthread_spin_lock(pthread_spinlock_t *lock) {
     while (1) {
         int i;
