c6777e6124e2458810e31cf92b43a2f75829c078
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-03-09 15:06:58 +0000

    Removes unused microbenchmarks

--- Micro_Benchmarks/GMCF_Message_Rate_Registers/gensrc/GMCF/Models/GMCFmodelF.h
+++
@@ -1,6 +0,0 @@
-// Generated wrapper for GMCF. Only parameter is $nmodels
-// GMCFmodelF.h
-#ifndef _GMCFMODELF_H_
-#define _GMCFMODELF_H_
-extern "C" void program_message_rate_(int64_t*,int64_t*, const int*);
-#endif // _GMCFMODELF_H_
