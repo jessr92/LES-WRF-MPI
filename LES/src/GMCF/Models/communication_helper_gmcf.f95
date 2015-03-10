74359871fc95fbe62336b1ff81bd1b9b7595ed46
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-03-10 10:48:55 +0000

    Updates LES global op to faster version

--- LES/src/GMCF/Models/communication_helper_gmcf.f95
+++ LES/src/GMCF/Models/communication_helper_gmcf.f95
@@ -4,6 +4,9 @@ use gmcfAPI
 implicit none
 
 integer :: mpi_size ! Names for compatibility with existing MPI code
+integer :: stillToWrite = 0
+integer :: stillToRead = 0
+real(kind=4) :: opResult = 0
 
 contains
 
@@ -331,106 +334,57 @@ subroutine getGlobalOp(model_id, value, tag)
     real(kind=4), intent(inout) :: value
     !print*, 'Model_id ', model_id, ' beginning getGlobalOp with tag ', tag
     if (isMaster()) then
-        call getGlobalOpMaster(model_id, value, tag)
+        call getGlobalOpMaster(value, tag)
     else
-        call getGlobalOpNotMaster(model_id, value, tag)
+        call getGlobalOpNotMaster(value, tag)
     end if
     !print*, 'Model_id ', model_id, ' finished getGlobalOp'
 end subroutine getGlobalOp
 
-subroutine getGlobalOpMaster(model_id, value, tag)
-    integer, intent(in) :: model_id, tag
+subroutine getGlobalOpMaster(value, tag)
+    integer, intent(in) :: tag
     real(kind=4), intent(inout) :: value
-    real(kind=4), dimension(1) :: receiveBuffer, sendBuffer
-    integer :: i, has_packets, fifo_empty
-    type(gmcfPacket) :: packet
-    ! Wait for responses
-    do i=2, mpi_size
-        call gmcfWaitFor(model_id, RESPDATA, i, 1)
-        call gmcfAddOneToSet(model_id, RESPDATA, i)
+    do while (stillToRead .ne. 0)
     end do
-    !print*, 'Model_id ', model_id, ' has received responses for every value'
-    ! Read in responses and find the min value
-    call gmcfHasPackets(model_id, RESPDATA, has_packets)
-    do while(has_packets == 1)
-        call gmcfShiftPendingFromInclusionSet(model_id, RESPDATA, packet, fifo_empty)
-        if (packet%data_id .ne. tag) then
-            print*, 'Received unexpected packet global op master RESPDATA'
-        else
-            call gmcfRemoveFromSet(model_id, RESPDATA, packet%source)
-            call gmcfRead1DFloatArray(receiveBuffer, shape(receiveBuffer), packet)
-            if (tag .eq. globalMinTag) then
-                if (receiveBuffer(1) .lt. value) then
-                    value = receiveBuffer(1)
-                end if
-            else if (tag .eq. globalMaxTag) then
-                if (receiveBuffer(1) .gt. value) then
-                    value = receiveBuffer(1)
-                end if
-            else if (tag .eq. globalSumTag) then
-                value = value + receiveBuffer(1)
-            else
-                print*, 'Unexpected global op'
-            end if
-        end if
-        call gmcfHasPackets(model_id, RESPDATA, has_packets)
-    end do
-    !print*, 'Model_id ', model_id, ' has every value and has done the op'
-    ! Wait for everybody to request the global sum
-    sendBuffer(1) = value
-    do i=2, mpi_size
-        call gmcfSend1DFloatArray(model_id, sendBuffer, shape(sendBuffer), tag, i, PRE, 1)
-    end do
-    ! Wait for acks
-    do i=2,mpi_size
-        call gmcfWaitFor(model_id, ACKDATA, i, 1)
-        call gmcfAddOneToSet(model_id, ACKDATA, i)
-        !print*, 'Model_id ', model_id, ' received final ack from ', i
-    end do
-    !print*, 'Model_id ', model_id, ' has receive all acks for new value'
-    ! Deal with the acks
-    call gmcfHasPackets(model_id, ACKDATA, has_packets)
-    do while(has_packets == 1)
-        call gmcfShiftPendingFromInclusionSet(model_id, ACKDATA, packet, fifo_empty)
-        call gmcfRemoveFromSet(model_id, ACKDATA, packet%source)
-        call gmcfHasPackets(model_id, ACKDATA, has_packets)
-    end do
+    call gmcfLockGlobalOpSpinLock()
+    stillToWrite = PROC_PER_ROW * PROC_PER_COL
+    stillToRead = PROC_PER_ROW * PROC_PER_COL
+    opResult = 0
+    call gmcfUnlockGlobalOpSpinLock()
+    call reduce(value, tag)
 end subroutine getGlobalOpMaster
 
-subroutine getGlobalOpNotMaster(model_id, value, tag)
-    integer, intent(in) :: model_id, tag
+subroutine getGlobalOpNotMaster(value, tag)
+    integer, intent(in) :: tag
     real(kind=4), intent(inout) :: value
-    real(kind=4), dimension(1) :: receiveBuffer, sendBuffer
-    integer :: has_packets, fifo_empty
-    type(gmcfPacket) :: packet
-    !print*, 'Model_id ', model_id, ' is an else thread'
-    sendBuffer(1) = value
-    call gmcfSend1DFloatArray(model_id, sendBuffer, shape(sendBuffer), tag, 1, PRE, 1)
-    !print*, 'Model_id ', model_id, ' has sent their value'
-    call gmcfWaitFor(model_id, ACKDATA, 1, 1)
-    ! Deal with the acks
-    call gmcfHasPackets(model_id, ACKDATA, has_packets)
-    do while(has_packets == 1)
-        call gmcfShiftPending(model_id, 1, ACKDATA, packet, fifo_empty)
-        call gmcfHasPackets(model_id, ACKDATA, has_packets)
-    end do        
-    ! Wait for response
-    call gmcfWaitFor(model_id, RESPDATA, 1, 1)
-    ! Read in response
-    call gmcfHasPackets(model_id, RESPDATA, has_packets)
-    do while(has_packets == 1)
-        call gmcfShiftPending(model_id, 1, RESPDATA, packet, fifo_empty)
-        if (packet%data_id .ne. tag) then
-            call gmcfPushPending(model_id, packet)
-        else
-            call gmcfRead1DFloatArray(receiveBuffer, shape(receiveBuffer),packet)
-            exit
-        end if
-        call gmcfHasPackets(model_id, RESPDATA, has_packets)
+    do while (stillToWrite .eq. 0)
     end do
-    ! Copy received value into appropriate place
-    value = receiveBuffer(1)
+    call reduce(value, tag)
 end subroutine getGlobalOpNotMaster
+
+subroutine reduce(value, tag)
+    integer, intent(in) :: tag
+    real(kind=4), intent(inout) :: value
+    call gmcfLockGlobalOpSpinLock()
+    if (tag .eq. globalSumTag) then
+        opResult = opResult + value
+    else if (tag .eq. globalMaxTag) then
+        if (value .gt. opResult) then
+            opResult = value
+        end if
+    else if (tag .eq. globalMinTag) then
+        if (value .lt. opResult) then
+            opResult = value
+        end if    
+    else
+        print*, 'Unknown tag ', tag
+    end if
+    stillToWrite = stillToWrite - 1
+    call gmcfUnlockGlobalOpSpinLock()
+    do while (stillToWrite .ne. 0)
+    end do
+    value = opResult
+end subroutine reduce
 
 subroutine recv3DReal4Array(rank, i, recvBuffer, bufferSize)
     real(kind=4), dimension(:,:,:), intent(out) :: recvBuffer
