c0488cf30526e8eece692d04866b7a42869083fb
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-03-26 15:37:29 +0000

    Adds R version of MPICH/OpenMPI comparison graphs

---
+++ Project Paper/graphs/MPICH313-OpenMPI184-fixed-area.r
@@ -0,0 +1,23 @@
+data <- read.table("MPICH313-OpenMPI184-fixed-area.input", header=T, sep="\t")
+
+y_range = range(20, data$mpich, data$openmpi)
+x_range = range(data$processes)
+
+plot_colours <- c("blue", "red")
+
+png(filename="MPICH313-OpenMPI184-fixed-area.png", height=350, width=500, bg="white")
+
+plot(data$processes, data$mpich, log="xy",type="l", col=plot_colours[1], ylim=y_range, xlim=x_range, axes=TRUE, ann=FALSE)
+
+lines(data$processes, data$openmpi, type="l", col=plot_colours[2], ylim=y_range, xlim=x_range, ann=FALSE)
+
+abline(v=c(1, 2, 5, 10, 20, 50), col="grey10", lty="dotted")
+abline(h=c(20, 50, 100, 200), col="grey10", lty="dotted")
+
+legend("topright", legend = c("MPICH 3.1.3", "OpenMPI 1.8.4"), fill=plot_colours)
+
+title(main="MPICH 3.1.3 versus OpenMPI 1.8.4 Fixed Area")
+title(xlab="Process count")
+title(ylab="Runtime (s)")
+
+box()
