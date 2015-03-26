5b4d87407e6f9342e71cf62e3db1a06d0ac30eff
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2015-03-26 15:19:24 +0000

    Adds R version of MPICH Expanding Area graph

--- Project Paper/graphs/MPICH313-expanding-area.r
+++ Project Paper/graphs/MPICH313-expanding-area.r
@@ -1,22 +1,22 @@
-data <- read.table("MPICH313-fixed-area.input", header=T, sep="\t")
+data <- read.table("MPICH313-expanding-area.input", header=T, sep="\t")
 
-y_range = range(20, data$default, data$bycore)
+y_range = range(100, data$default, data$bycore)
 x_range = range(data$processes)
 
 plot_colours <- c("blue", "red")
 
-png(filename="MPICH313-fixed-area.png", height=350, width=500, bg="white")
+png(filename="MPICH313-expanding-area.png", height=350, width=500, bg="white")
 
 plot(data$processes, data$default, log="xy",type="l", col=plot_colours[1], ylim=y_range, xlim=x_range, axes=TRUE, ann=FALSE)
 
 lines(data$processes, data$bycore, type="l", col=plot_colours[2], ylim=y_range, xlim=x_range, ann=FALSE)
 
 abline(v=c(1, 2, 5, 10, 20, 50), col="grey10", lty="dotted")
-abline(h=c(20, 50, 100, 200), col="grey10", lty="dotted")
+abline(h=c(100, 150, 200), col="grey10", lty="dotted")
 
-legend("topright", legend = c("Default mapping", "By core mapping"), fill=plot_colours)
+legend("bottomright", legend = c("Default mapping", "By core mapping"), fill=plot_colours)
 
-title(main="MPICH 3.1.3 Fixed Area")
+title(main="MPICH 3.1.3 Expanding Area")
 title(xlab="Process count")
 title(ylab="Runtime (s)")
 
